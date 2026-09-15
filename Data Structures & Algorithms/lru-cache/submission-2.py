4a36ce0e3a76e8ed50bf909b51b76a29bc0b10f7
Georgiy Savchenko <54809902+GeorgiySav@users.noreply.github.com>
2026-09-15 11:16:34 +0100

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,23 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.order = deque()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            value = self.cache[key]
+            self.order.remove((key, value))
+            self.order.append((key, value))
+            return value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache and len(self.cache) == self.capacity:
+            k, v = self.order.popleft()
+            self.cache.pop(k)
+        elif key in self.cache:
+            self.order.remove((key, self.cache[key]))
+        self.order.append((key, value))
+        self.cache[key] = value
